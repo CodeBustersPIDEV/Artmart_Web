fa6053d6b3610952a571292a0c136d5f6876a523
AmirCW <80427194+MedAmirSoltani@users.noreply.github.com>
2023-03-27 14:12:43 +0100

    fix

--- src/Form/CustomproductType.php
+++ src/Form/CustomproductType.php
@@ -2,16 +2,10 @@
 
 namespace App\Form;
 
-use App\Entity\Categories;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Entity\Customproduct;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-
-
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class CustomproductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
