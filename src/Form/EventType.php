c37d37c6bd046eb5a4bfb018630b5658b8c53649
Ghassen-Zayoud <ghassen.zayoud@esprit.tn>
2023-04-06 02:50:13 +0100

    added crud for all entities + integrate  navbar

---
+++ src/Form/EventType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('location')
+            ->add('type')
+            ->add('description')
+            ->add('entryfee')
+            ->add('capacity')
+            ->add('startdate')
+            ->add('enddate')
+            ->add('image')
+            ->add('status')
+            ->add('userid')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
