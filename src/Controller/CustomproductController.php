6e8e6aa140d74110f9b05f21ff9b234ba7594ed6
AmirCW <80427194+MedAmirSoltani@users.noreply.github.com>
2023-04-09 03:57:10 +0100

    backend+ bundle (pagination)

--- src/Controller/CustomproductController.php
+++ src/Controller/CustomproductController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Customproduct;
+use App\Entity\Categories;
+
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Knp\Component\Pager\PaginatorInterface;
 
 use App\Form\CustomproductType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -21,7 +24,7 @@ use App\Entity\User;
 class CustomproductController extends AbstractController
 {
     #[Route('/', name: 'app_customproduct_index', methods: ['GET'])]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
     {
         $searchTerm = $request->query->get('q');
         $order = $request->query->get('order');
@@ -42,15 +45,66 @@ class CustomproductController extends AbstractController
             $queryBuilder->where('p.name LIKE :searchTerm')
                 ->setParameter('searchTerm', '%' . $searchTerm . '%');
         }
+        $pagination = $paginator->paginate(
+            $queryBuilder->getQuery(),
+            $request->query->getInt('page', 1),
+            6  );
         
         $customproducts = $queryBuilder->getQuery()->getResult();
     
         return $this->render('customproduct/index.html.twig', [
-            'customproducts' => $customproducts,
+            'customproducts' => $pagination,
             'searchTerm' => $searchTerm,
             'order' => $order,
         ]);
+        
     }
+
+
+    #[Route('/admin', name: 'app_customproduct_admin', methods: ['GET'])]
+    public function adminindex(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $searchTerm = $request->query->get('q');
+        $order = $request->query->get('order');
+        $categories = $entityManager
+        ->getRepository(Categories::class)
+        ->findAll();
+        $applies = $entityManager
+        ->getRepository(Apply::class)
+        ->findAll();
+        $queryBuilder = $entityManager
+            ->getRepository(Customproduct::class)
+            ->createQueryBuilder('c')
+            ->innerJoin('c.product', 'p');
+            
+        
+        if ($order === 'name') {
+            $queryBuilder->orderBy('p.name', 'ASC');
+        } elseif ($order === 'weight') {
+            $queryBuilder->orderBy('p.weight', 'ASC');
+        }
+        
+        if ($searchTerm) {
+            $queryBuilder->where('p.name LIKE :searchTerm')
+                ->setParameter('searchTerm', '%' . $searchTerm . '%');
+        }
+        
+        $customproducts = $queryBuilder->getQuery()->getResult();
+    
+        return $this->render('customproduct/admin.html.twig', [
+            'customproducts' => $customproducts,
+            'searchTerm' => $searchTerm,
+            'order' => $order,
+            'categories' => $categories,
+            'applies' => $applies,
+        ]);
+        
+    }
+
+
+
+
+
     #[Route('/customproduct', name: 'app_customproduct_artist', methods: ['GET'])]
     public function artist(EntityManagerInterface $entityManager): Response
     {
