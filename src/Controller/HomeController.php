9e83ec67e7eec8de13a026013e7ba89b6a44c1b1
AmirCW <80427194+MedAmirSoltani@users.noreply.github.com>
2023-04-10 18:48:14 +0100

    front done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Customproduct;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+    public function index(EntityManagerInterface $entityManager): Response
+    { $queryBuilder = $entityManager
+        ->getRepository(Customproduct::class)
+        ->createQueryBuilder('c')
+        ->innerJoin('c.product', 'p');
+        $customproducts = $queryBuilder->getQuery()->getResult();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'customproducts' => $customproducts,
         ]);
     }
     #[Route('/Panel', name: 'app_admin')]
