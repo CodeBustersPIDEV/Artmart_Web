cb55465cb82a453d291e872360a1dedabe34c02d
samar0507 <79337944+samar0507@users.noreply.github.com>
2023-04-12 01:51:25 +0100

    register without test

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,7 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
+use App\Entity\Artist;
+use App\Entity\Admin;
+use App\Entity\Client;
+use App\Repository\ArtistRepository;
+use App\Repository\ClientRepository;
+use App\Repository\UserRepository;
+use App\Repository\AdminRepository;
 use App\Security\UserAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,37 +18,102 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function uploadImage(UploadedFile $file, User $user): void
     {
+        $destinationFilePath = $this->getParameter('destinationPath');
+        // Get the original filename of the uploaded file
+        $filename = $file->getClientOriginalName();
+        if (!is_uploaded_file($file->getPathname())) {
+            throw new FileException('File was not uploaded via HTTP POST.');
+        }
+
+        if (!is_dir($destinationFilePath)) {
+            // Create the directory
+            mkdir($destinationFilePath, 0777, true);
+        }
+        // Move the uploaded file to the destination
+        $file->move($destinationFilePath, $filename);
+        $user->setPicture($destinationFilePath . $filename);
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager, UserRepository $userRepository, ArtistRepository $artistRepository, ClientRepository $clientRepository, AdminRepository $adminRepository): Response
+    {
+       
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $addedUser = new User();
+        $artist = new Artist();
+        $admin = new Admin();
+
+        $form = $this->createForm(UserType::class, $user, [
+            'is_edit' => false,
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
+            $role = $form->get('role')->getData();
+            $user = $form->getData();
+            $file = $form->get('file')->getData();
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
+            $email = $form->get('email')->getData();
+            $client = new Client();
+            $this->uploadImage($file, $user);
+
+            // $user->setRole($role);
+            $userRepository->save($user, true);
+
+            if ($role === 'client') {
+                $addedUser = $userRepository->findOneUserByEmail($email);
+                $client->setUser($addedUser);
+                $clientRepository->save($client, true);
+            } elseif ($role === 'artist') {
+                $addedUser = $userRepository->findOneUserByEmail($email);
+                $artist->setUser($addedUser);
+                $artistRepository->save($artist, true);
+            } elseif ($role === 'admin') {
+                $addedUser = $userRepository->findOneUserByEmail($email);
+                $admin->setUser($addedUser);
+                $adminRepository->save($admin, true);
+            }
+
+            if ($role === 'client') {
+                // $user = new Client();
+                $client->setNbrDemands(0);
+                $client->setNbrOrders(0);
+                // $client->setUserId($userId);
+                $client->setUser($user);
+                $entityManager->persist($client);
+                $entityManager->flush();
+            } elseif ($role == 'artist') {
+
+                $artist->setNbrArtwork(0);
+                //  $artist->setUserId($userId);
+                $artist->setUser($user);
+                $entityManager->persist($artist);
+                $entityManager->flush();
+            } elseif ($role == 'admin') {
+
+                //  $admin->setUserId($userId);
+                $admin->setUser($user);
+                $entityManager->persist($admin);
+                $entityManager->flush();
+            }
+
+        
 
             return $userAuthenticator->authenticateUser(
                 $user,
                 $authenticator,
                 $request
             );
+        
         }
-
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -9,38 +9,68 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
+        ->add('name', TextType::class, [
+            'required' => true,
+        ])
+        ->add('email', EmailType::class, [
+            'required' => true,
+        ])
+        ->add('birthday', DateType::class)
+        ->add('phonenumber')
+        
+        ->add('username', TextType::class, [
+            'required' => true,
+        ])
+       
+            ->add('username')
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('role', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'admin',
+                    'Artist' => 'artist',
+                    'Client' => 'client',
+                ],
                 'required' => true,
             ])
-            ->add('email', EmailType::class, [
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
                 'required' => true,
-            ])
-            ->add('birthday', DateType::class)
-            ->add('phonenumber')
-            
-            ->add('username', TextType::class, [
-                'required' => true,
+                'first_options'  => [
+                    'label' => 'Password',
+                    'attr' => ['class' => 'form-control', 'id' => 'password']
+                ],
+                'second_options' => [
+                    'label' => 'Confirm Password',
+                    'attr' => ['class' => 'form-control', 'id' => 'confirm_password']
+                ],
             ])
             ->add('file', FileType::class, [
                 'label' => 'Choose a file',
                 'mapped' => false,
                 'required' => true,
             ])
-            ->add('Are you selling art pieces?', ChoiceType::class, [
-                'choices' => [
-                    'Yes' => 'yes',
-                    'No' => 'no',
-                ],
-                'required' => true,
-            ]);
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
