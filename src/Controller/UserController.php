d8547f4c3acc19f0dbfe4731a06a6d1807f9e881
samar0507 <79337944+samar0507@users.noreply.github.com>
2023-04-13 03:02:39 +0100

    frontoffice+blackoffice css +login not complete

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -27,7 +27,9 @@ class UserController extends AbstractController
 
     public function uploadImage(UploadedFile $file, User $user): void
     {
-        $destinationFilePath = $this->getParameter('destinationPath');
+        $destinationFilePath = $this->getParameter ('destinationPath');
+        $newdestinationFilePath=$this->getParameter ('file_base_url');
+        $filePath = sprintf('%s/%s', $newdestinationFilePath['host'], $newdestinationFilePath['path']);
         // Get the original filename of the uploaded file
         $filename = $file->getClientOriginalName();
         if (!is_uploaded_file($file->getPathname())) {
@@ -40,7 +42,7 @@ class UserController extends AbstractController
         }
         // Move the uploaded file to the destination
         $file->move($destinationFilePath, $filename);
-        $user->setPicture($destinationFilePath . $filename);
+        $user->setPicture($filePath.'/'. $filename);
     }
 
 
@@ -52,16 +54,16 @@ class UserController extends AbstractController
 
         $queryBuilder = $entityManager
             ->getRepository(User::class)
-            ->createQueryBuilder('c');
+            ->createQueryBuilder('u');
 
         if ($userse === 'name') {
-            $queryBuilder->orderBy('c.name', 'ASC');
+            $queryBuilder->orderBy('u.name', 'ASC');
         } elseif ($userse === 'username') {
-            $queryBuilder->orderBy('c.username', 'ASC');
+            $queryBuilder->orderBy('u.username', 'ASC');
         }
 
         if ($searchTerm) {
-            $queryBuilder->where('c.username LIKE :searchTerm')
+            $queryBuilder->where('u.username LIKE :searchTerm')
                 ->setParameter('searchTerm', '%' . $searchTerm . '%');
         }
 
@@ -141,7 +143,7 @@ class UserController extends AbstractController
                 $entityManager->flush();
             }
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_user_new', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('user/new.html.twig', [
@@ -187,6 +189,48 @@ class UserController extends AbstractController
             ]);
         } elseif ($role === 'admin') {
             return $this->render('user/show.html.twig', [
+                'user' => $user,
+                'adminAttributes' => $adminAttributes ?? null,
+            ]);
+        }
+    }
+    #[Route('/{userId}/Profile', name: 'app_user_Profile', methods: ['GET'])]
+    public function Profile(User $user, ClientRepository $clientRepository, ArtistRepository $artistRepository, AdminRepository $adminRepository): Response
+    {
+        $client = $clientRepository->findOneBy(['user' => $user]);
+        $artist = $artistRepository->findOneBy(['user' => $user]);
+        $admin = $adminRepository->findOneBy(['user' => $user]);
+
+        $role = $user->getRole();
+
+        if ($role === 'client' && $client) {
+            $clientAttributes = [
+                'nbrOrders' => $client->getNbrOrders(),
+                'nbrDemands' => $client->getNbrDemands(),
+            ];
+        } elseif ($role === 'artist' && $artist) {
+            $artistAttributes = [
+                'bio' => $artist->getBio(),
+                'nbrArtwork' => $artist->getNbrArtwork(),
+            ];
+        }
+        if ($role === 'admin' && $admin) {
+            $adminAttributes = [
+                'department' => $admin->getDepartment(),
+            ];
+        }
+        if ($role === 'client') {
+            return $this->render('user/Profile.html.twig', [
+                'user' => $user,
+                'clientAttributes' => $clientAttributes ?? null,
+            ]);
+        } elseif ($role === 'artist') {
+            return $this->render('user/Profile.html.twig', [
+                'user' => $user,
+                'artistAttributes' => $artistAttributes ?? null,
+            ]);
+        } elseif ($role === 'admin') {
+            return $this->render('user/Profile.html.twig', [
                 'user' => $user,
                 'adminAttributes' => $adminAttributes ?? null,
             ]);
@@ -265,6 +309,87 @@ class UserController extends AbstractController
         }
 
         return $this->renderForm('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+            'is_edit' => true,
+            'client_attributes' => $clientAttributes,
+            'artist_attributes' => $artistAttributes,
+            'admin_attributes' => $adminAttributes,
+        ]);
+    }
+
+    #[Route('/{userId}/editProfile', name: 'app_user_editProfile', methods: ['GET', 'POST'])]
+    public function editProfile(Request $request, User $user, EntityManagerInterface $entityManager, ClientRepository $clientRepository, ArtistRepository $artistRepository, AdminRepository $adminRepository): Response
+    {
+        $client = $clientRepository->findOneBy(['user' => $user]);
+        $artist = $artistRepository->findOneBy(['user' => $user]);
+        $admin = $adminRepository->findOneBy(['user' => $user]);
+        $role = $user->getRole();
+
+        $clientAttributes = [
+            'nbrOrders' => null,
+            'nbrDemands' => null,
+        ];
+        $artistAttributes = [
+            'bio' => null,
+            'nbrArtwork' => null,
+        ];
+        $adminAttributes = [
+            'department' => null,
+        ];
+
+        if ($role === 'artist' && $artist) {
+            $artistAttributes = [
+                'bio' => $artist->getBio(),
+            ];
+        } elseif ($role === 'admin' && $admin) {
+            $adminAttributes = [
+                'department' => $admin->getDepartment(),
+            ];
+        }
+
+        $form = $this->createForm(UserType::class, $user, [
+            'is_edit' => true,
+            'client_attributes' => $clientAttributes,
+            'artist_attributes' => $artistAttributes,
+            'admin_attributes' => $adminAttributes,
+        ]);
+
+
+        if ($role === 'artist' && $artist) {
+            $bioField = $form->get('bio');
+            $bioField->setData($artist->getBio());
+        }
+
+        // Set the initial value of the department field if the user is an admin
+        if ($role === 'admin' && $admin) {
+            $departmentField = $form->get('department');
+            $departmentField->setData($admin->getDepartment());
+        }
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $pictureField = $form->get('file')->getData();
+            if ($pictureField == null) {
+                $user->setPicture($user->getPicture());
+            } else {
+                $this->uploadImage($pictureField, $user);
+            }
+            if ($user->getRole() === 'artist' && $artist) {
+                $bio = $form->get('bio')->getData();
+                $artist->setBio($bio);
+                $entityManager->persist($artist);
+            } elseif ($user->getRole() === 'admin' && $admin) {
+                $department = $form->get('department')->getData();
+                $admin->setDepartment($department);
+                $entityManager->persist($admin);
+            }
+
+            $entityManager->flush();
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/editF.html.twig', [
             'user' => $user,
             'form' => $form,
             'is_edit' => true,
