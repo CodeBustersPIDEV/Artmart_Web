beb5282e6db89510dbc8064dbd6bece74d228f21
MarwenTouati1 <marwen.touati@esprit.tn>
2023-04-28 00:28:42 +0100

    Rating UI

--- src/Form/CommentsType.php
+++ src/Form/CommentsType.php
@@ -3,41 +3,30 @@
 namespace App\Form;
 
 use App\Entity\Comments;
-use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use blackknight467\StarRatingBundle\Form\RatingType;
-
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
 
 class CommentsType extends AbstractType
 {
-    private UserRepository $UserRepository;
-
-
-    public function __construct(UserRepository $UserRepository)
-    {
-        $this->UserRepository = $UserRepository;
-    }
-
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('content')
-            ->add('rating');
-        // ->add('author', EntityType::class, [
-        //     'class' => User::class,
-        //     'choices' => $this->UserRepository->findAll(),
-        //     'choice_label' => 'name',
-        //     'placeholder' => 'Choose an option',
-        //     'choice_attr' => function ($choice, $key, $value) {
-        //         return ['style' => 'color:black;'];
-        //     }
-        // ])
+            // ->add('rating');
+            ->add('rating', ChoiceType::class, [
+                'choices' => [
+                    '★' => 1,
+                    '★★' => 2,
+                    '★★★' => 3,
+                    '★★★★' => 4,
+                    '★★★★★' => 5,
+                ],     'label_html' => false,
 
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
