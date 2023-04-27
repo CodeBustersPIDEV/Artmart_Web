719cf2f56b6c21d9496e14bc0cb4bc451c729153
AmirCW <80427194+MedAmirSoltani@users.noreply.github.com>
2023-04-27 09:30:46 +0100

    bon

--- config/bundles.php
+++ config/bundles.php
@@ -15,7 +15,6 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
-    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
-
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
 ];
