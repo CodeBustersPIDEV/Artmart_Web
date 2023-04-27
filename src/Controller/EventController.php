71008cef983bd637ca199c30838dd99567d0092f
Ghassen-Zayoud <ghassen.zayoud@esprit.tn>
2023-04-27 16:14:28 +0100

    updated event MVC to utilize user session

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -6,6 +6,7 @@ use App\Entity\Event;
 use App\Entity\User;
 use App\Form\EventType;
 use App\Repository\EventRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,10 +15,23 @@ use Symfony\Component\Routing\Annotation\Route;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 #[Route('/event')]
 class EventController extends AbstractController
 {
+    private User $connectedUser;
+
+    public function __construct(SessionInterface $session, UserRepository $userRepository)
+    {
+        if ($session != null) {
+            $connectedUserID = $session->get('user_id');
+            if (is_int($connectedUserID)) {
+                $this->connectedUser = $userRepository->find((int) $connectedUserID);
+            }
+        }
+    }
+
     public function uploadImage(UploadedFile $file, Event $event): void
     {
         $destinationFilePath = $this->getParameter ('destinationPath');
@@ -38,6 +52,49 @@ class EventController extends AbstractController
         $event->setImage($filePath.'/'. $filename);
     }
 
+    #[Route('/', name: 'app_event_index', methods: ['GET'])]
+    public function indexVisitor(EntityManagerInterface $entityManager, EventRepository $eventRepository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $searchTerm = $request->query->get('q');
+        $name = $request->query->get('name');
+        $feeOrder = $request->query->get('feeOrder');
+        $status = $request->query->get('status');
+        $type = $request->query->get('type');
+        
+        $events = $eventRepository->findAll();
+
+        // if ($name) {
+        //     $events = $eventRepository->findAllSortedByName($name);
+        // }
+        // elseif ($feeOrder) {
+        //     $events = $eventRepository->findAllSortedByPrice($feeOrder);
+        // }
+        // elseif ($status) {
+        //     $events = $eventRepository->findByStatus($status);
+        // }
+        // elseif ($type) {
+        //     $events = $eventRepository->findByType($type);
+        // }
+        // else if ($searchTerm) {
+        //     $events = $eventRepository->findByTerm($searchTerm);
+        // }
+
+        $pages = $paginator->paginate(
+            $events, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            9 // Nombre de résultats par page
+        );
+
+        return $this->render('event/artist/index.html.twig', [
+            'events' => $pages,
+            'searchTerm' => $searchTerm,
+        ]);
+    }
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+     /////////////////////////////////////////////// ADMIN //////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     #[Route('/admin', name: 'app_event_index_admin', methods: ['GET'])]
     public function indexAdmin(EntityManagerInterface $entityManager, EventRepository $eventRepository, PaginatorInterface $paginator, Request $request): Response
     {
@@ -57,21 +114,21 @@ class EventController extends AbstractController
         if ($userID) {
             $events = $eventRepository->findByUser($userID);
         }
-        if ($name) {
-            $events = $eventRepository->findAllSortedByName($name);
-        }
-        elseif ($feeOrder) {
-            $events = $eventRepository->findAllSortedByPrice($feeOrder);
-        }
-        elseif ($status) {
-            $events = $eventRepository->findByStatus($status);
-        }
-        elseif ($type) {
-            $events = $eventRepository->findByType($type);
-        }
-        else if ($searchTerm) {
-            $events = $eventRepository->findByTerm($searchTerm);
-        }
+        // if ($name) {
+        //     $events = $eventRepository->findAllSortedByName($name);
+        // }
+        // elseif ($feeOrder) {
+        //     $events = $eventRepository->findAllSortedByPrice($feeOrder);
+        // }
+        // elseif ($status) {
+        //     $events = $eventRepository->findByStatus($status);
+        // }
+        // elseif ($type) {
+        //     $events = $eventRepository->findByType($type);
+        // }
+        // else if ($searchTerm) {
+        //     $events = $eventRepository->findByTerm($searchTerm);
+        // }
 
         $pages = $paginator->paginate(
             $events, // Requête contenant les données à paginer (ici nos articles)
@@ -183,72 +240,125 @@ class EventController extends AbstractController
         return $this->redirectToRoute('app_event_index_admin', [], Response::HTTP_SEE_OTHER);
     }
     
-    /////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////
-    /////////////////////////////////////////////////////
+       ///////////////////////////////////////////////        /////////////////////////////////////////////////
+      ///////////////////////////////////////////////        /////////////////////////////////////////////////
+     /////////////////////////////////////////////// ARTIST /////////////////////////////////////////////////
+    ///////////////////////////////////////////////        /////////////////////////////////////////////////
+   ///////////////////////////////////////////////        /////////////////////////////////////////////////
 
     #[Route('/artist', name: 'app_event_index_artist', methods: ['GET'])]
     public function index(EntityManagerInterface $entityManager, EventRepository $eventRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        $userID = $request->query->get('userID');
+        $connectedUserID = $this->connectedUser->getUserId();
+        $connectedUserRole = $this->connectedUser->getRole();
 
-        $searchTerm = $request->query->get('q');
-        $name = $request->query->get('name');
-        $feeOrder = $request->query->get('feeOrder');
-        $status = $request->query->get('status');
-        $type = $request->query->get('type');
+        $showOtherEvents = $request->query->get('showOtherEvents') == 1;
 
-        $users = $entityManager
-            ->getRepository(User::class)
-            ->findAll();
+        if ($connectedUserRole=== "artist") {
+
+            $searchTerm = $request->query->get('q');
+            $name = $request->query->get('name');
+            $feeOrder = $request->query->get('feeOrder');
+            $status = $request->query->get('status');
+            $type = $request->query->get('type');
         
-        $events = $eventRepository->findAll();
-       
-        if ($userID) {
-            $events = $eventRepository->findByUser($userID);
-        }
-        if ($name) {
-            $events = $eventRepository->findAllSortedByName($name);
-        }
-        elseif ($feeOrder) {
-            $events = $eventRepository->findAllSortedByPrice($feeOrder);
-        }
-        elseif ($status) {
-            $events = $eventRepository->findByStatus($status);
-        }
-        elseif ($type) {
-            $events = $eventRepository->findByType($type);
-        }
-        else if ($searchTerm) {
-            $events = $eventRepository->findByTerm($searchTerm);
-        }
+            if ($request->query->get('view') === 'otherEvents') {
+                $showOtherEvents = true;
+            }
+        
+            if (!$showOtherEvents) {
+                $events = $eventRepository->findByUser($connectedUserID);
+            } else {
+                $events = $entityManager
+                    ->getRepository(Event::class)
+                    ->createQueryBuilder('e')
+                    ->andWhere('e.user != :val')
+                    ->setParameter('val', $this->connectedUser->getUserId())
+                    ->getQuery()
+                    ->getResult();
+            }
+        
+            if ($name) {
+                $events = $eventRepository->findAllSortedByName($name, $connectedUserID, $showOtherEvents);
+            } elseif ($feeOrder) {
+                $events = $eventRepository->findAllSortedByPrice($feeOrder, $connectedUserID, $showOtherEvents);
+            } elseif ($status) {
+                $events = $eventRepository->findByStatus($status, $connectedUserID,$showOtherEvents);
+            } elseif ($type) {
+                $events = $eventRepository->findByType($type, $connectedUserID,$showOtherEvents);
+            } elseif ($searchTerm) {
+                $events = $eventRepository->findByTerm($searchTerm, $connectedUserID,$showOtherEvents);
+            }
 
-        $pages = $paginator->paginate(
-            $events, // Requête contenant les données à paginer (ici nos articles)
-            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
-            9 // Nombre de résultats par page
-        );
-
-        return $this->render('event/artist/index.html.twig', [
-            'events' => $pages,
-            'users' => $users,
-            // 'userID' => $userID,
-            'searchTerm' => $searchTerm,
-        ]);
+            $pages = $paginator->paginate(
+                $events,
+                $request->query->getInt('page', 1),
+                9
+            );
+        
+            return $this->render('event/artist/index.html.twig', [
+                'events' => $pages,
+                'searchTerm' => $searchTerm,
+                'showOtherEvents' => $showOtherEvents,
+            ]);
+        // } else {
+        //     return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
+        }
     }
+    
 
-    #[Route('/artist/otherEvents/{id}', name: 'app_event_otherEvents_artist', methods: ['GET'])]
-    public function findOtherEvents(EntityManagerInterface $entityManager, $id): Response
-    {
-        $events = $entityManager
-            ->getRepository(Event::class)
-            ->findOtherEvents($id);
+    // #[Route('/artist/otherEvents', name: 'app_event_other_events_artist', methods: ['GET'])]
+    // public function findOtherEvents(EntityManagerInterface $entityManager, EventRepository $eventRepository, PaginatorInterface $paginator, Request $request): Response
+    // {
+    //     $connectedUserID = $this->connectedUser->getUserId();
+    //     $connectedUserRole = $this->connectedUser->getRole();
 
-        return $this->render('event/artist/index.html.twig', [
-            'events' => $events,
-        ]);
-    }
+    //     $showOtherEvents = true;
 
+    //     if ($connectedUserRole=== "artist") {
+            
+    //         $searchTerm = $request->query->get('q');
+    //         $name = $request->query->get('name');
+    //         $feeOrder = $request->query->get('feeOrder');
+    //         $status = $request->query->get('status');
+    //         $type = $request->query->get('type');
+
+    //         $events = $entityManager
+    //             ->getRepository(Event::class)
+    //             ->createQueryBuilder('e')
+    //             ->andWhere('e.user != :val')
+    //             ->setParameter('val', $this->connectedUser->getUserId())
+    //             ->getQuery()
+    //             ->getResult();
+
+    //         if ($name) {
+    //             $events = $eventRepository->findAllSortedByName($name, $connectedUserID, $showOtherEvents);
+    //         } elseif ($feeOrder) {
+    //             $events = $eventRepository->findAllSortedByPrice($feeOrder, $connectedUserID, $showOtherEvents);
+    //         } elseif ($status) {
+    //             $events = $eventRepository->findByStatus($status, $connectedUserID,$showOtherEvents);
+    //         } elseif ($type) {
+    //             $events = $eventRepository->findByType($type, $connectedUserID,$showOtherEvents);
+    //         } elseif ($searchTerm) {
+    //             $events = $eventRepository->findByTerm($searchTerm, $connectedUserID,$showOtherEvents);
+    //         }
+        
+    //         $pages = $paginator->paginate(
+    //             $events,
+    //             $request->query->getInt('page', 1),
+    //             9
+    //         );
+        
+    //         return $this->render('event/artist/otherEvents.html.twig', [
+    //             'events' => $pages,
+    //             'searchTerm' => $searchTerm,
+    //             'showOtherEvents' => $showOtherEvents,
+    //         ]);
+    //     } else {
+    //         return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
+    //     }
+    // }
+    
     #[Route('/artist/new', name: 'app_event_new_artist', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
