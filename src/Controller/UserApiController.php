f75fdc8492e17809cdfdc4c57f571995cbb6e82b
samar0507 <79337944+samar0507@users.noreply.github.com>
2023-05-11 17:09:01 +0100

    loginapi

--- src/Controller/UserApiController.php
+++ src/Controller/UserApiController.php
@@ -34,14 +34,12 @@ class UserApiController extends AbstractController
         $responseArray = array();
         foreach ($user as $user) {
             $responseArray[] = array(
-                'User_id' => $user->getUserId(),
+                'user_id'=>$user->getUserId(),
                 'name' => $user->getName(),
                 'username' => $user->getUsername(),
                 'phonenumber' => $user->getPhonenumber(),
                 'email' => $user->getEmail(),
                 'password' => $user->getPassword(),
-                'enabled' => $user->isEnabled(),
-                'blocked' => $user->isBlocked(),
                 'birthday' => $user->getBirthday(),
                 'role' => $user->getRole(),
                 'file'=>$user->getPicture()
@@ -63,20 +61,19 @@ class UserApiController extends AbstractController
         $client = new Clients();
         $artist = new Artist();
         $hashedPassword = hash('sha256', $request->request->get('password'));
-        $token = bin2hex(random_bytes(16));
-        $user->setToken($token);
+        $code = mt_rand(1000, 9999);
+        $user->setToken($code);
         $user->setName($request->request->get('name'));
         $user->setRole($request->request->get('role'));
         $user->setUsername($request->request->get('username'));
         $user->setPassword($hashedPassword);
-        $birthday = \DateTime::createFromFormat('Y-m-d', $request->request->get('birthday'));
-        $user->setBirthday($birthday);
-        $user->setPicture($request->request->get('file'));
+        $user->setBirthday(new \DateTime($request->request->get('birthday')));
         $user->setEmail($request->request->get('email'));
         $user->setPhonenumber($request->request->get('phonenumber'));
-
+        $user->setPicture("http://localhost/PIDEV/BlogUploads/user.png");
         $entityManager->persist($user);
         $entityManager->flush();
+
         if ($user->getRole() === 'client') {
             $addedUser = $userRepository->findOneUserByEmail($user->getEmail());
             $client->setUser($addedUser);
@@ -92,54 +89,11 @@ class UserApiController extends AbstractController
         }
 
 
-
         $response = new JsonResponse(['status' => 'added'], Response::HTTP_CREATED);
         return $response;
     }
-    #[Route('/user/addAd', name: 'user_AD', methods: ['GET', 'POST'])]
-    public function adduserAd(Request $request, UserRepository $userRepository, ArtistRepository $artistRepository, ClientRepository $clientRepository, AdminRepository $adminRepository): JsonResponse
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-        $user = new User();
-        $admin = new Admin();
-        $client = new Clients();
-        $artist = new Artist();
-        $hashedPassword = hash('sha256', $request->request->get('password'));
-        $token = bin2hex(random_bytes(16));
-        $user->setToken($token);
-        $user->setName($request->request->get('name'));
-        $user->setRole('admin');
-        $user->setUsername($request->request->get('username'));
-        $user->setPassword($hashedPassword);
-        $birthday = \DateTime::createFromFormat('Y-m-d', $request->request->get('birthday'));
-        $user->setBirthday($birthday);
-        $user->setPicture($request->request->get('file'));
-        $user->setEmail($request->request->get('email'));
-        $user->setPhonenumber($request->request->get('phonenumber'));
-
-        $entityManager->persist($user);
-        $entityManager->flush();
-        if ($user->getRole() === 'client') {
-            $addedUser = $userRepository->findOneUserByEmail($user->getEmail());
-            $client->setUser($addedUser);
-            $clientRepository->save($client, true);
-        } elseif ($user->getRole() === 'artist') {
-            $addedUser = $userRepository->findOneUserByEmail($user->getEmail());
-            $artist->setUser($addedUser);
-            $artistRepository->save($artist, true);
-        } elseif ($user->getRole() === 'admin') {
-            $addedUser = $userRepository->findOneUserByEmail($user->getEmail());
-            $admin->setUser($addedUser);
-            $adminRepository->save($admin, true);
-        }
-
-
-
-        $response = new JsonResponse(['status' => 'added'], Response::HTTP_CREATED);
-        return $response;
-    }
-
-    #[Route('/user/{id}', name: 'user_edit', methods: ['PUT'])]
+   
+    #[Route('/user/edit/{id}', name: 'user_edit', methods: ['PUT'])]
     public function edituser(Request $request, User $user, ArtistRepository $artistRepository, AdminRepository $adminRepository): JsonResponse
     {
 
@@ -151,25 +105,20 @@ class UserApiController extends AbstractController
         if (!$user) {
             return new JsonResponse(['status' => 'Failed']);
         }
-    
+
+       
         if ($role === 'artist' && $artist) {
-            $artist->setBio($request->request->get('bio'));
-            $entityManager->persist($artist);
-
+        $artist->getBio($request->request->get('bio'));
         }
-    
+
+        // Set the initial value of the department field if the user is an admin
         if ($role === 'admin' && $admin) {
-            $admin->setDepartment($request->request->get('department'));
-            $entityManager->persist($admin);
-
+            $admin->getDepartment($request->request->get('department'));
         }
-    
         $user->setName($request->request->get('name'));
         $user->setUsername($request->request->get('username'));
         $user->setPhonenumber($request->request->get('phonenumber'));
-        $birthday = \DateTime::createFromFormat('Y-m-d', $request->request->get('birthday'));
-        $user->setBirthday($birthday);
-        $user->setPicture($request->request->get('file'));
+        $user->setBirthday(new \DateTime($request->request->get('birthday')));
         $user->setEmail($request->request->get('email'));
 
         $entityManager->persist($user);
@@ -178,81 +127,61 @@ class UserApiController extends AbstractController
         $response = new JsonResponse(['status' => 'edited'], Response::HTTP_OK);
         return $response;
     }
-
-    public function signinAction(Request $request){
-
+    #[Route('/user/signin', name: 'user_login', methods: ['POST'])]
+    public function signinAction(Request $request)
+    {
         $username = $request->query->get("username");
-        $password = $request->query->get("password");
 
-        $em= $this->getDoctrine()->getManager();
-        $user= $em->getRepository(User::class)->findOneBy(['username'=>$username]);// find user by email
-
-        if($user){
-            if(password_verify($password,$user->getPassword())) {
-                 $serializer = new Serializer([new ObjectNormalizer()]);
+        $hashedPassword = hash('sha256', $request->request->get('password'));
+       
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository(User::class)->findOneBy(['username' => $username]);
+    
+        if ($user) {
+            if ($hashedPassword== $user->getPassword()) {
+                $serializer = new Serializer([new ObjectNormalizer()]);
                 $formatted = $serializer->normalize($user);
-                return new JsonResponse($formatted);
+                return new JsonResponse(['success' => true, 'data' => $formatted]);
+            } else {
+                return new JsonResponse(['success' => false, 'message' => $hashedPassword]);
             }
-            else{
-                return new Response("password not found");
-            }
-
-            }
-        else{
-            return new Response("failed");
-
+        } else {
+            return new JsonResponse(['success' => false, 'message' => 'User not found']);
         }
     }
+    
 
 
-    public function editpassword(Request $request, UserRepository $userRepository): Response
-    {      $hashedPassword = hash('sha256', $request->query->get("password"));
-        
-        $email = $request->query->get("email");
-        
-               
-        $em=$this->getDoctrine()->getManager();
-        $user =$this->getDoctrine()->getManager()->getRepository(User::class)->findOneBy(['email'=>$email]);
-       
-           $user->setPassword($hashedPassword);
-           $user->setEmail($email);
-        
-           
-                   
-       try {
-           $em = $this->getDoctrine()->getManager ();
-           $em->persist($user);
-           $em->flush();
-           return new JsonResponse("Password was updates successfully",200);//290 ya3ni http result tas server OK
-       }catch (\Exception $ex) {
-           return new Response("failed ".$ex->getMessage());
-       }
+    #[Route('/user/delete/{id}', name: 'user_delete', methods: ['POST','DELETE'])]
+    public function delete( User $user, ClientRepository $clientRepository, ArtistRepository $artistRepository, AdminRepository $adminRepository): Response
+    {
+        if (!$user) {
+            throw $this->createNotFoundException('The user does not exist');
+        }
+        $entityManager = $this->getDoctrine()->getManager();
+        if ($user->getRole() == 'client') {
+            $client = $clientRepository->findOneBy(['user' => $user]);
+            $entityManager->remove($client);
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+        if ($user->getRole() == 'artist') {
+            $artist = $artistRepository->findOneBy(['user' => $user]);
+            $entityManager->remove($artist);
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+        if ($user->getRole() == 'admin') {
+            $admin = $adminRepository->findOneBy(['user' => $user]);
+            $entityManager->remove($admin);
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+
+
+
+        $response = new JsonResponse(['status' => 'deleted'], Response::HTTP_OK);
+        return $response;
     }
-
-
-    public function delete(Request $request, User $user, ClientRepository $clientRepository, ArtistRepository $artistRepository, AdminRepository $adminRepository): Response
-    { if (!$user) {
-        throw $this->createNotFoundException('The user does not exist');
-    } $entityManager = $this->getDoctrine()->getManager();
-        if($user->getRole()=='admin'){
-        $client = $clientRepository->findOneBy(['user' => $user]);
-        $entityManager->remove($client);
-        $entityManager->flush();
-        } if($user->getRole()=='admin'){
-        $artist = $artistRepository->findOneBy(['user' => $user]);
-        $entityManager->remove($artist);
-        $entityManager->flush();
-    } if($user->getRole()=='admin'){
-        $admin = $adminRepository->findOneBy(['user' => $user]);
-        $entityManager->remove($admin);
-        $entityManager->flush();
-    }
-       
-       
-        
-
-    $response = new JsonResponse(['status' => 'deleted'], Response::HTTP_OK);
-    return $response;
-    }
-
 }
